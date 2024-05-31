08e1d3f4f0e088a6586c88a23295a7bfc28b7a91
bypo <oreste@dimaggio.it>
2024-05-31 18:35:33 +0200

    Added file
    The function now supports the ability to select chronicle bigquery instance by specifying Customer Code (TLA) in the query string.
    This parameter is now mandatory.

--- chronicle-silent-entities-cf/index.js
+++ chronicle-silent-entities-cf/index.js
@@ -1,17 +1,3 @@
-// Copyright 2022 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 // [START chronicleSilentEntities]
 // Import the Google Cloud client library
 const {BigQuery} = require('@google-cloud/bigquery');
@@ -26,22 +12,23 @@ const functions = require('@google-cloud/functions-framework');
  * @param {Object} res Cloud Function response context.
  */
 functions.http('chronicleSilentEntities', async (req, res) => {
+  // Define parameters
+  const sec_late = (req.query.sec_late || 600);
+  const limit_count = (req.query.limit_count || 100 );
+  const chronicle_tla = (req.query.chronicle_tla || 'none' );
+  if(chronicle_tla=='none')
+     res.status(500).send(`Error 'chronicle_tla' http parameter not specified`);
+
   // Define the SQL queries
   const sqlQuery1 = `
-      SELECT DISTINCT events__intermediary.hostname as gateway, FROM \`chronicle-ita.datalake.events\` as events LEFT JOIN UNNEST(events.intermediary) AS events__intermediary
-      WHERE metadata.ingested_timestamp.seconds > @sec_late  ORDER BY gateway LIMIT @limit_count;`
-  const sqlQuery2 = `
-      SELECT principal.hostname FROM \`chronicle-ita.datalake.events\` WHERE metadata.ingested_timestamp.seconds > @sec_late LIMIT @limit_count;`
-
+      SELECT principal.hostname as gateway, MAX(metadata.event_timestamp.seconds) as maxtime, count(*) FROM chronicle-${chronicle_tla}.datalake.events as events
+      WHERE DATE(hour_time_bucket) > DATE_SUB(CURRENT_DATE(), INTERVAL 3 DAY) group by 1 having count(*) > 1000 
+      and (unix_seconds(current_timestamp()) - maxtime ) > ${sec_late} 
+      ORDER BY gateway 
+      LIMIT ${limit_count}`
+  
   const options1 = {
     query: sqlQuery1,
-    // Location must match that of the dataset(s) referenced in the query.
-    location: 'EU',
-    params: {sec_late: 300, limit_count: 400},
-  };
-
-  const options2 = {
-    query: sqlQuery2,
     // Location must match that of the dataset(s) referenced in the query.
     location: 'EU',
     params: {sec_late: 300, limit_count: 400},
@@ -54,17 +41,8 @@ functions.http('chronicleSilentEntities', async (req, res) => {
     res.status(200).send(rows);
   } catch (err) {
     console.error(err);
-    res.status(500).send(`Error querying BigQuery (query1): ${err}`);
-  }
-
-  // Execute the query
-  try {
-    const [rows] = await bigquery.query(options2);
-    // Send the results
-    res.status(200).send(rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(`Error querying BigQuery (query2): ${err}`);
+    res.status(500).send(`Error querying BigQuery (query1): ${err} and ${sqlQuery1}`);
+    
   }
 
 });
